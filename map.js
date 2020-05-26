c56e2854a11ce06c9ff2ddd9ba1725842fa68d6a
sda782 <59613370+sda782@users.noreply.github.com>
2020-05-26 16:35:06 +0200

    first working test with markers and images

--- map.js
+++ map.js
@@ -1,19 +1,35 @@
-var map = L.map('map').fitWorld();
+var map = L.map('map', { zoomControl: false }).fitWorld();
+var imageindex;
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: '',
-    maxZoom: 16,
-    id: 'mapbox/dark-v10',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiYWV2dW0iLCJhIjoiY2thanI0c2FlMDkxaDJ0bHM4Z2pwNGllcyJ9.Od2H02EIte0aVMCRR5IuvQ'
 }).addTo(map);
 
-map.locate({ setView: true, maxZoom: 16 });
+$('.leaflet-control-attribution').toggle();
 
+map.locate({ setView: true, maxZoom: 18 });
+
+$.getJSON('https://raw.githubusercontent.com/sda782/zenze/master/imageindex.json', (Iindex) => {
+    console.log('test');
+
+    Object.keys(Iindex).forEach(function(k) {
+        var item = Iindex[k];
+        console.log(item);
+        var marker = L.marker([item.coord.latitude, item.coord.longitude]).addTo(map);
+        var photoImg = '<img src="' + item.images[0] + '" height="150px" width="150px"/>';
+        marker.bindPopup("<b>" + item.title + "</b><br><p>" + item.description + "</p>" + photoImg);
+    });
+});
+
+//find you
 function onLocationFound(e) {
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+    L.marker(e.latlng).addTo(map);
 }
 
 map.on('locationfound', onLocationFound);
